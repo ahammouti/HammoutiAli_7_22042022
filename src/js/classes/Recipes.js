bf746aab33b350cfd4f2d8d2400d63aa0215a8f2
Hammouti Ali <ali.hammouti@epsi.fr>
2022-06-27 17:14:19 +0200

    fix : fix error and refactor of the code

--- src/js/classes/Recipes.js
+++ src/js/classes/Recipes.js
@@ -1,17 +1,6 @@
 export default class Recipes {
-
-    constructor(recipes) {
-
+    constructor() {
         this.galleryRecipes = document.getElementsByClassName("js-galleryRecipes")[0];
-        // this.bindEvent();
-        // this.displayRecipes(recipes);
-        // this.attachEvent();
-    }
-
-    bindEvent() {
-    }
-
-    attachEvent() {
     }
 
     displayRecipes(arrayRecipes) {
@@ -53,38 +42,3 @@ export default class Recipes {
         };
     }
 };
-
-{/* <article class="cardRecipes">
-                <div class="cardRecipes__imgRecipe"></div>
-                <div class="cardRecipes__overlayRecipe">
-                    <div class="heading">
-                        <h3 class="heading__title">Poisson Cru à la tahitienne</h3> <!-- ! dynamique -->
-                        <span class="heading__timer">
-                            <img src="./src/images/icons/timer.svg" alt="timer">
-                            <small>60 min</small> <!-- ! dynamique -->
-                        </span>
-                    </div>
-                    <div class="ingredientsAndDescription">
-                        <div class="ingredientsAndDescription__ingr">
-                            <ul>
-                                <li>
-                                    <div>Lait de coco : <span>400ml ddzdddzd dad</span> </div> <!-- ! dynamique -->
-                                </li>
-                                <li>
-                                    <div>ingredient : <span>value</span> </div>
-                                </li>
-                                <li>
-                                    <div>ingredient : <span>value</span> </div>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="ingredientsAndDescription__desc">
-                            <!-- ! dynamique -->
-                            <p>Découper le thon en dés, mettre dans un plat et recouvrir de jus de citron vert (mieux
-                                vaut prendre un plat large et peu profond). Laisser reposer au réfrigérateur au moins 2
-                                heures. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quos!
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </article> */}
